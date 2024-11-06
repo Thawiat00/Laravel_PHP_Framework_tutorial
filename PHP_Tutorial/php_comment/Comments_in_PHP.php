4014ebe59267b8ec4a758420d3093c2dc566b77d
Thawiat00 <Thawiat01@gmail.com>
2024-11-07 01:30:32 +0700

    add php_comment
    add php comment

---
+++ PHP_Tutorial/php_comment/Comments_in_PHP.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Syntax for comments in PHP code:";
+      echo "</br>";
+
+    echo "// This is a single-line comment";
+    echo "</br>";
+    echo "# This is also a single-line comment";
+    echo "</br>";
+    echo "/* This is a";
+    echo "</br>";
+   echo  "multi-line comment */";
+   echo "</br>";
+   echo "</br>";
+    ?>
+
+
+    <?php
+    echo "Example";
+    echo "</br>";
+    echo "A comment at the end of a line:";
+    echo "</br>";
+    echo "Welcome Home!"; echo "// Outputs a welcome message";
+    echo "</br>";
+    echo "</br>";
+    ?>
+
+<?php
+$string=" '' ";
+
+    echo "Comments to Ignore Code";
+    echo "</br>";
+    echo "Example";
+    echo "</br>";
+    echo "Do not display a welcome message:";
+    echo "</br>";
+    echo "//". $string ."echo Welcome Home!".$string;
+
+?>
+
+</body>
+</html>
